19e48b027d438645d26e2606dbd0671a7295408c
Azona Isagba <isagbaazona@gmail.com>
2025-04-23 01:02:58 -0700

    TodoShow component

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,7 +1,17 @@
 import React from "react";
+import TodoShow from "./TodoShow";
 
-const TodoList = () => {
-    return <div>Todo List</div>;
+const TodoList = ({ todos, removeTodo, changeTodo }) => {
+    const renderedTodos = todos.map((todo) => {
+        return (
+            <TodoShow key={todo.id}
+            todo= {todo}
+            removeTodo={removeTodo}
+            changeTodo={changeTodo}
+            />
+        );
+    });
+    return <ul className="todo-list">{renderedTodos}</ul>
 };
 
 export default TodoList;
--- src/component/TodoShow.js
+++ src/component/TodoShow.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import TodoEdit from "./TodoEdit";
+import { MdDelete } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
 
-const TodoShow = () => {
-    return  <div>Todo Show</div>;
+const TodoShow = ({todo, removeTodo, changeTodo }) => {
+   const [showEdit, setShowEdit] = useState(false);
+   const handleDelete = (e) => {
+    removeTodo(todo.id);
+   };
+
+   const handleEdit = (e) => {
+    setShowEdit(true);
+   };
+
+   const handleDblClick = (e) => {
+    changeTodo(todo.id, todo.title, !todo.completed);
+   };
+
+   const handleSubmit = (id, title) => {
+    changeTodo(id, title);
+    setShowEdit(false);
+   };
+
+   if (showEdit) {
+    return (
+      <li className="todo">
+        <TodoEdit todo={todo} onSubmit={handleSubmit} />
+      </li>
+    );
+  }
+  return (
+    <li className="todo" onDoubleClick={handleDblClick}>
+      <p className={todo.completed && 'completed'}>{todo.title}</p>
+  
+      <div className="actions">
+        <button onClick={handleDelete}>
+        <MdDelete />
+        </button>
+        <button onClick={handleEdit}>
+        <FaRegEdit />
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default TodoShow;
