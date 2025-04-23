869516f18114dc704b9e5849f448b5cd0155fd63
Azona Isagba <isagbaazona@gmail.com>
2025-04-23 02:22:35 -0700

    Added css file

--- src/component/TodoEdit.js
+++ src/component/TodoEdit.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaCheck } from "react-icons/fa";
 
-const TodoEdit = () => {
-    return <div>Todo Edit</div>;
+
+const TodoEdit = ({ todo, onSubmit }) => {
+  const [title, setTitle] = useState(todo.title);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(todo.id, title);
+  };
+
+  return (
+    <form className="todo-edit">
+      <input type="text" value={title} onChange={handleChange} />
+      <button type="submit" onClick={handleSubmit}>
+      <FaCheck />
+      </button>
+    </form>
+  );
 };
 
 export default TodoEdit;
